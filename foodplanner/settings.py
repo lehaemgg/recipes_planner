2af54fa0fd364e7042586b095bba19959b597968
Your Name <you@example.com>
2025-12-01 22:34:34 +0100

    setting for pythonanywhere

--- foodplanner/settings.py
+++ foodplanner/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zg%yd3r%xnn3ky7x8y0@ow+c20=%tg=lg+--e&l-w3ba&y-twi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['lehaem.eu.pythonanywhere.com', 'localhost', '127.0.0.1']
 
@@ -116,14 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/lehaem/recipes_planner/static'
+STATIC_ROOT = '/home/lehaem/foodplanner/static'
 
 # Media files
 MEDIA_URL = '/media/'
-if DEBUG:
-    MEDIA_ROOT = BASE_DIR / 'media'
-else:
-    MEDIA_ROOT = '/home/lehaem/recipes_planner/media'
+MEDIA_ROOT = '/home/lehaem/foodplanner/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
